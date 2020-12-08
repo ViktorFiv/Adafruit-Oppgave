872ab4aeeb042904d2ee458b4f1d445fefe3cc86
ViktorFiv <74405302+ViktorFiv@users.noreply.github.com>
2020-12-08 13:27:04 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,17 +1,42 @@
 import time
 from Adafruit_IO import Client, Feed, RequestError
 import pyfirmata
+import mysql.connector
 
 run_count = 0
 ADAFRUIT_IO_USERNAME = "ViktorFiv"
-ADAFRUIT_IO_KEY = "aio_OsWu67eJQTKWI7zNwJ0uMKBMjwSQ"
+ADAFRUIT_IO_KEY = "aio_tPzc01fbTkKeQerZkJwVk8zzFLWk"
+
+mydb = mysql.connector.connect(
+	host="localhost",
+	user="root",
+	password="Vkitor76",
+	database="mydb"
+)
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
 board = pyfirmata.Arduino('COM7')
-
+ 
 it = pyfirmata.util.Iterator(board)
 it.start()
+
+mycursor = mydb.cursor()
+
+print("connected")
+
+sql= "INSERT INTO sensor(verdi, tid) VALUES (%s%s)"
+verdi = 1.0
+tid = datetime.datetime.now()
+
+val = (verdi, tid)
+
+print("Executing...")
+
+mycursor.execute(sql, val)
+mydb.commit()
+
+print("Done")
 
 digital_output = board.get_pin('d:13:o')
 
@@ -34,4 +59,5 @@ while True:
 		digital_output.write(True)
 	else:
 		digital_output.write(False)
+	
 	time.sleep(2)
